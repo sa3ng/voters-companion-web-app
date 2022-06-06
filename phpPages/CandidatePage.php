4915daec1e7a5d5c4b88c64ee4339f209cd2b5d7
Edgar Cerda <epicerds2@gmail.com>
2022-06-06 09:06:33 +0800

    removed redundant bulma line

--- phpPages/CandidatePage.php
+++ phpPages/CandidatePage.php
@@ -57,7 +57,6 @@ if ($candidate_basic["position_id"] == "P") {
     <title>Voters' Companion</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 
-    <link rel='stylesheet prefetch' href='https://unpkg.com/bulma@0.9.0/css/bulma.min.css'>
     <link rel="stylesheet" href="../resources/css/tabs.css">
     <link rel="stylesheet" href="../resources/css/voterscompanion.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
