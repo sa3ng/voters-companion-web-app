2f1abf3ca7a369dc520acc7355b058f8e998afd8
PAU PAU <sa3ngbut1l@gmail.com>
2022-07-04 15:01:53 +0800

    mano mano merge - test 1

--- phpPages/ForumPage.php
+++ phpPages/ForumPage.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
   <link rel="stylesheet" href="../resources/css/voterscompanion.css">
 
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
   <script src="https://kit.fontawesome.com/7dc3015a44.js" crossorigin="anonymous"></script>
 
   <style>
@@ -43,10 +43,11 @@
 
   
   //Initialize functions
-  $post_headers = fetchHeaders($DB_CREDENTIALS); 
+  $post_headers = fetchHeaders($DB_CREDENTIALS);
+  //$post_likes = fetchLikes($DB_CREDENTIALS);
   $post_acc_ids = fetchPostAccID($DB_CREDENTIALS);
   $post_dates = fetchDates($DB_CREDENTIALS, 1);
-  $post_ids = fetchIDs($DB_CREDENTIALS);
+
   //Pending Posts - Users
   $posts_pending_headers = fetchPendingPosts($DB_CREDENTIALS, 'header');
   $posts_pending_dates = fetchPendingPosts($DB_CREDENTIALS, 'date');
@@ -73,17 +74,6 @@
   .post-icons {
     padding-right: 10px;
   }
-
-  .button-to-link {
-    background: none;
-    border: none;
-    padding: 0;
-
-    /*input has OS specific font-family*/
-    color: #069;
-    cursor: pointer;
-  }
-
 
   .transparent {
         cursor: pointer;
@@ -238,8 +228,7 @@
       $index = 0;
       foreach ($post_headers as $headers) {
         $user_tag_array = fetchTags($DB_CREDENTIALS, $post_acc_ids, $num_of_posts);
-        $post_likes = fetchLikes($DB_CREDENTIALS, $post_id[$num_of_posts]);
-        
+
         echo "
       
         <form action='ForumPostPage.php' method='POST'>
@@ -258,13 +247,12 @@
             <div class='media-content'>
               <div class='content'>
                 <p>
-                  <button formaction='usrPage.php' class='button-to-link' type='submit'>" . $user_tag_array[0] . "</button> posted on $post_dates[$num_of_posts]
+                  <a href='#'>" . $user_tag_array[0] . "</a> posted on $post_dates[$num_of_posts]
                   <span class='tag'>Question</span>
                 </p>
               </div>
             </div>
-            <div class='media-right'>
-            <span><button type='button' id ='btn' class='transparent post-icons has-text-grey-light' onclick='setColor('btn')'><i class='fa-solid fa-thumbs-up'></i> " . $post_likes . "</button></span>";
+            <div class='media-right'>";
 
             if(strcmp($post_acc_type,"editor") == 0){
               echo"<button class='button is-danger' formaction='../phpScripts/delete_post.php' type='submit'><i class='fa-solid fa-xmark'></i>&nbsp; Remove</button>";
@@ -286,20 +274,7 @@
     </div>
   </div>
   </div>
-  <script>
-    var count = 1;
-    function setColor(btn) {
-        var property = document.getElementById(btn);
-        if (count == 0) {
-            property.style.backgroundColor = "#FFFFFF"
-            count = 1;        
-        }
-        else {
-            property.style.backgroundColor = "#24A0ED"
-            count = 0;
-        }
-    }
-  </script>
+
 </body>
 
 </html>
